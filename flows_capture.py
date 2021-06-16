483eb30432c550c82aa677670796d810d73ce179
tpacini <t.pacini2@studenti.unipi.it>
2021-06-16 17:21:57 +0200

    Add command line arguments

--- flows_capture.py
+++ flows_capture.py
@@ -1,26 +1,49 @@
 import nfstream
-
-DEBUG = 0
-
-# Print some infos of the received flows 
-def print_formatted_flows(my_streamer):
-    for flow in my_streamer:
-        print("{0:3d}: {1:15s} : {2:5d} --> {3:15s} : {4:5d} | A_name: {5:15s} R_s_n: {6:20s}".format(
-            flow.id,
-            flow.src_ip,
-            flow.src_port,
-            flow.dst_ip,
-            flow.dst_port,
-            flow.application_name,
-            flow.requested_server_name))
+import sys, getopt
 
 # Export the received flows in the ".csv" format
-def print_pandas_flows(my_streamer):
+def print_pandas_flows(my_streamer, out_file):
     df = my_streamer.to_pandas()
-    df.to_csv('./test1_60min.csv')
+    df.to_csv('./' + out_file + '.csv')
+
+# Parse command-line arguments
+def parse_cmdline_args(argv):
+    interface = "null"
+    out_file = "null"
+
+    # No arguments/flag in input
+    if len(argv) == 0:
+        print("Usage: flows_capture.py -i <capture_interface> -o <output_filename>")
+        sys.exit()
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["help=", "interface=", "output_filename="])
+    except getopt.GetoptError:
+        print("Error")
+        sys.exit(2)
+    
+    # Parsing arguments
+    for opt, arg in opts:
+        if opt in ['-h', '--help']:
+            print("Usage: flows_capture.py -i <capture_interface> -o <output_filename>")
+            sys.exit()
+        elif opt in ['-i', '--interface']:
+            interface = arg
+        elif opt in ['-o', '--output_filename']:
+            out_file = arg
+
+    if interface == "null" or out_file == "null":
+        print("Usage: flows_capture.py -i <capture_interface> -o <output_filename>")
+        sys.exit()
+
+    return interface, out_file
 
 if __name__ == "__main__":
-    my_streamer = nfstream.NFStreamer(source="wlp1s0",
+    # Get command-line arguments
+    interface, out_file = parse_cmdline_args(sys.argv[1:])
+
+    # Activate metering processes
+    my_streamer = nfstream.NFStreamer(source=interface,
             bpf_filter=None,
             snapshot_length=1600,
             idle_timeout=120,
@@ -28,7 +51,4 @@ if __name__ == "__main__":
             udps=None,
             statistical_analysis=False)
 
-    if DEBUG == 1:
-        print_formatted_flows(my_streamer)
-    else:
-        print_pandas_flows(my_streamer)
+    print_pandas_flows(my_streamer, out_file)
